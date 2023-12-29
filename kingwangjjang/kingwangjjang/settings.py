ba12e8fa6005a5716289f86470a96bd8d6822b80
Hwanju <hwanju1596@gmail.com>
2023-12-29 11:52:22 +0900

    test: test app init

--- kingwangjjang/kingwangjjang/settings.py
+++ kingwangjjang/kingwangjjang/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "graphene_django" # Graph ql
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
